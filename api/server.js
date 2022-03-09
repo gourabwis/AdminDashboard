30eda6ba765cad55d2db79c734fef990aafa6d8b
Gourab Neogi <gourab.wis@gmail.com>
2022-03-09 18:32:29 +0530

    authentication added

--- api/server.js
+++ api/server.js
@@ -1,14 +1,40 @@
 const express = require("express");
 const cors = require("cors");
 const Product = require("./config");
+const cookieParser = require("cookie-parser");
+const serviceAccount = require("./config/serviceAccount.json");
+const admin = require("./config/firebase.config");
+const csrf = require("csurf");
 const app = express();
 app.use(express.json());
 app.use(cors());
+const csrfMiddleware = csrf({ cookie: true });
+const port = process.env.PORT || 4000;
+app.use(cookieParser());
+app.use(csrfMiddleware);
+
+app.all("*", (req, res, next) => {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  next();
+});
+
 
 app.get("/", async (req, res) => {
-  const snapshot = await Product.get();
+  const sessionCookie = req.cookies.session || "";
+
+  await admin
+    .auth()
+    .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then((userData) => {
+      console.log("Logged in:", userData.email)
+  console.log(req.headers);
+  const snapshot = Product.get();
   const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
   res.send(list);
+})
+.catch((error) => {
+  console.log("Error:", error);
+});
 });
 
 app.post("/create", async (req, res) => {
@@ -31,4 +57,33 @@ app.post("/delete", async (req, res) => {
   res.send({ msg: "Deleted" });
 });
 
-app.listen(4000, () => console.log(`Server started on port 4000`));
+app.post("/sessionLogin", (req, res) => {
+  const idToken = req.body.idToken.toString();
+
+  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+
+  admin
+    .auth()
+    .createSessionCookie(idToken, { expiresIn })
+    .then(
+      (sessionCookie) => {
+        const options = { maxAge: expiresIn, httpOnly: true };
+        res.cookie("session", sessionCookie, options);
+        res.end(JSON.stringify({ status: "success" }));
+      },
+      (error) => {
+        res.status(401).send("UNAUTHORIZED REQUEST!");
+      }
+    );
+});
+
+app.get("/login", function (req, res) {
+  res.render("login.html");
+});
+
+app.get("/sessionLogout", (req, res) => {
+  res.clearCookie("session");
+  res.redirect("/login");
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
